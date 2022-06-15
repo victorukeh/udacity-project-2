4d05e66d2c98eddaff18a81bf1e8f4f3b5e9f08a
Victor .U. Ukeh <38099933+victorukeh@users.noreply.github.com>
2022-06-15 08:43:03 +0200

    deployed

---
+++ src/server.ts
@@ -0,0 +1,66 @@
+import express from "express";
+import path from "path";
+import { Request, Response } from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+
+(async () => {
+	// Init the Express application
+	const app = express();
+
+	// Set the network port
+	const port = process.env.PORT || 8082;
+
+	// Use the body parser middleware for post requests
+	app.use(bodyParser.json());
+
+	// @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+	// GET /filteredimage?image_url={{URL}}
+	// endpoint to filter an image from a public url.
+	// IT SHOULD
+	//    1
+	//    1. validate the image_url query
+	//    2. call filterImageFromURL(image_url) to filter the image
+	//    3. send the resulting file in the response
+	//    4. deletes any files on the server on finish of the response
+	// QUERY PARAMATERS
+	//    image_url: URL of a publicly accessible image
+	// RETURNS
+	//   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+	/**************************************************************************** */
+
+	//! END @TODO1
+	app.get("/filteredimage", async (req: Request, res: Response) => {
+		const image: string = req.query.image_url;
+		if (!image)
+			return res.status(400).send({ message: "Image URL is required" });
+		try {
+			var file: string = await filterImageFromURL(image);
+		} catch (err) {
+			return res.status(422).send({ message: "Could not process request" });
+		}
+		res.sendFile(file, (err) => {
+			if (err) {
+				console.error(err);
+        return res
+        .status(500)
+        .send("The image is not accessible. Please check the URL and try again.")
+			}
+ 
+			deleteLocalFiles([file]);
+		});
+	});
+
+	// Root Endpoint
+	// Displays a simple message to the user
+	app.get("/", async (req: Request, res: Response) => {
+		res.send("try GET /filteredimage?image_url={{}}");
+	});
+
+	// Start the Server
+	app.listen(port, () => {
+		console.log(`server running http://localhost:${port}`);
+		console.log(`press CTRL+C to stop server`);
+	});
+})();
